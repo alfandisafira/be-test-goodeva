625ec630683f558a252aac1ea82e729329044b81
alfandisafira <alfandisafira@gmail.com>
2026-02-18 19:38:53 +0700

    feat: todos route

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query, Search } from '@nestjs/common';
 import { TodosService } from './todos.service';
 
-@Controller()
+@Controller('api/todos')
 export class TodosController {
-  constructor(private readonly todosService: TodosService) {}
+  constructor(private readonly todosService: TodosService) { }
+  
+  /**
+   * GET /api/todos?search= → daftar todo (array), serta filter sederhana pada title
+   * POST /api/todos → buat todo { title: string }
+   * PATCH /api/todos/:id →  created, completed, on_going, problem (tambahkan problem_desc)
+   */
+
+  @Get()
+  find(@Query('search') search?: string) {
+    return search ?? [];
+  }
+
+  @Post()
+  create(@Body() todo: { title: string }) {
+    return todo;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() todo: { title: string, status: string }) {
+    return { id, ...todo };
+  }
 }
